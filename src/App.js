8a46bbcec7b41ee9222e58d23b90f586753e158b
Moustapha Diakhaby <diakhabymous@gmail.com>
2023-02-01 15:06:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import minus from "./img/minus.png";
+import plus from "./img/plus.png";
+import calculator from "./img/calculator.png";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo">
+          <img src={calculator} alt="" />
+        </div>
+        <h1>React Counter</h1>
       </header>
+      <div className="main container">
+        <div className="counterbox">
+          <div
+            onClick={() => {
+              setCounter(counter - 1);
+            }}
+            className={counter <= 0 ? "hidden" : "minus"}
+          >
+            <img src={minus} alt="" />
+          </div>
+          <div className="number">{counter}</div>
+          <div
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+            className={counter >= 10 ? "hidden" : "plus"}
+          >
+            <img src={plus} alt="" />
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            setCounter(0);
+          }}
+          className="reset"
+        >
+          Reset
+        </button>
+        <p className="sign">
+          Made with <span>React</span> at <span>Le Reacteur</span> by{" "}
+          <span>Moustapha</span>
+        </p>
+      </div>
     </div>
   );
 }
